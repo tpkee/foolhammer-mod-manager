87848788eb985d1a9c2d53b11105c55cb8e2d95c
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-07 22:35:34 +0100

    feat: add Steam client and directory initialization with error handling

--- src-tauri/src/defaults/system.rs
+++ src-tauri/src/defaults/system.rs
@@ -1,11 +1,28 @@
 use lazy_static::lazy_static;
 use std::path::PathBuf;
+use steamlocate::SteamDir;
 lazy_static! {
     /// This is an example for using doc comment attributes
-    pub static ref PROGRAM_FILES_PATH: PathBuf = match std::env::consts::OS {
+    pub static ref PROGRAM_FILES_PATH: PathBuf = match std::env::consts::OS { // TODO: this is pointless now, remove asap
         "windows" => std::env::var("FOLDERID_ProgramFiles")
             .expect("Failed to get ProgramFiles environment variable")
             .into(),
         _ => dirs::data_dir().expect("Failed to get data directory"),
+    };
+
+    pub static ref STEAMWORKS_CLIENT_INSTANCE: Option<steamworks::Client> = match steamworks::Client::init() {
+        Ok(client) => Some(client),
+        Err(e) => {
+            eprintln!("Failed to initialize Steam client: {:?}", e);
+            None
+        }
+    };
+
+    pub static ref STEAMDIR_INSTANCE: Option<SteamDir> = match SteamDir::locate() { // this is useful to lookup apps and paths even if the steam client isn't running
+        Ok(steam_dir) => Some(steam_dir),
+        _ => {
+            eprintln!("Failed to locate Steam directory");
+            None
+        }
     };
 }
