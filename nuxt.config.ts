9da6b9f55e7d14d41f390a4a587f650ca9e2dc08
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-09 00:56:42 +0100

    feat: add @vueuse/nuxt module to enhance functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2026-02-05',
-  modules: ['@nuxt/a11y', '@nuxt/eslint', '@nuxt/hints', '@nuxt/icon', '@nuxtjs/i18n', '@pinia/nuxt'],
+  modules: ['@nuxt/a11y', '@nuxt/eslint', '@nuxt/hints', '@nuxt/icon', '@nuxtjs/i18n', '@pinia/nuxt', '@vueuse/nuxt'],
   srcDir: 'src/',
   // Enables the development server to be discoverable by other devices when running on iOS physical devices
   devServer: {
