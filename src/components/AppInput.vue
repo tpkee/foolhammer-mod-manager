1828b5501241bdcc8155b6ea4f6a6de423bc9a35
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-08 19:14:07 +0100

    fix: add focus-within styling to input and select components for better accessibility

--- src/components/AppInput.vue
+++ src/components/AppInput.vue
@@ -7,7 +7,7 @@
     >
       {{ label }}
     </span>
-    <input :id="uid" v-model="model" class="px-1.5 py-1 bg-gray-800 appearance-none border  placeholder:italic placeholder:text-gray-500 border-gray-700 focus:outline-none hover:border-purple-600 transition-all transition-duration-300 rounded disabled:opacity-75" v-bind="$attrs">
+    <input :id="uid" v-model="model" class="px-1.5 py-1 bg-gray-800 appearance-none border  placeholder:italic placeholder:text-gray-500 border-gray-700 focus:outline-none focus-within:border-purple-600 hover:border-purple-600 transition-all transition-duration-300 rounded disabled:opacity-75" v-bind="$attrs">
   </label>
 </template>
 
--- src/components/AppSelect.vue
+++ src/components/AppSelect.vue
@@ -7,7 +7,7 @@
     >
       {{ label }}
     </span>
-    <select :id="uid" v-model="model" class="px-1.5 py-1 bg-gray-800 border border-gray-700 appearance-none focus:outline-none hover:border-purple-600 transition-all transition-duration-300 rounded disabled:opacity-75" v-bind="$attrs">
+    <select :id="uid" v-model="model" class="px-1.5 py-1 bg-gray-800 border border-gray-700 appearance-none focus:outline-none focus:border-purple-600 hover:border-purple-600 transition-all transition-duration-300 rounded disabled:opacity-75" v-bind="$attrs">
       <option v-for="option in options" :key="option.value" :value="option.value" :disabled="option.disabled">
         {{ option.label }}
       </option>
