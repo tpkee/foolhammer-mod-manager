87b2dfd7f257e34b1d38dc565dd182d394eb9020
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-08 18:33:02 +0100

    feat: add ItemMod and ListMods components with layout and functionality

---
+++ src/components/ItemMod.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="grid grid-cols-12 p-2.5 items-center gap-2.5 text-left">
+    <div class="flex items-center gap-2.5 col-span-2">
+      <button class="cursor-grab active:cursor-move text-left">
+        <nuxt-icon name="mi:reorder" class="size-6" />
+      </button>
+      <app-input v-model.number="order" label="Order number" sr-only-label type="number" :min="1" class="w-20" />
+    </div>
+    <app-checkbox v-model="isEnabled" label="Is enabled?" sr-only-label />
+    <div class="flex items-center gap-2.5 col-span-3">
+      <div class="size-9 rounded">
+        <img v-if="image" :src="image" alt="" class="size-[inherit] rounded-[inherit] object-cover">
+        <div v-else class="size-[inherit] rounded-[inherit] bg-gray-700" />
+      </div>
+      <app-tooltip class="relative">
+        <template #content>
+          <p class="whitespace-nowrap">
+            {{ name }}
+          </p>
+        </template>
+        <p class="truncate">
+          {{ pack }}
+        </p>
+      </app-tooltip>
+    </div>
+    <time :datetime="getLastUpdate" class="text-xs text-gray-500 col-span-5">
+      {{ getLastUpdate }}
+    </time>
+
+    <app-options class="justify-self-end" :options="getOptions" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+// Props
+const props = defineProps<{
+  name: string
+  image?: string
+  pack: string
+  lastUpdate: string | Date | number
+}>()
+
+// Model
+const isEnabled = defineModel('enabled', { type: Boolean, required: true })
+const order = defineModel('order', { type: Number, required: true })
+
+// Computed
+const getLastUpdate = computed(() => {
+  const date = new Date(props.lastUpdate)
+  return date.toLocaleDateString()
+})
+const getOptions = computed(() => {
+  return [
+    {
+      icon: 'mi:delete',
+      label: 'Delete from profile',
+      callback: () => {
+        console.log('Delete from profile')
+      },
+    },
+    {
+      icon: 'mi:close',
+      label: 'Delete from disk',
+      callback: () => {
+        console.log('Delete from disk')
+      },
+    },
+  ]
+})
+</script>
---
+++ src/components/ListMods.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="border overflow-hidden rounded border-gray-700">
+    <div class="grid grid-cols-12 p-2.5 border border-gray-800 items-center gap-2.5 text-left [&>p]:whitespace-nowrap bg-gray-800">
+      <div class="flex items-center gap-2.5 col-span-2">
+        <p class="ml-9">
+          Order
+        </p>
+      </div>
+      <p>
+        Enabled?
+      </p>
+      <p class="col-span-3 ml-11">
+        Pack
+      </p>
+      <p>
+        Last update
+      </p>
+    </div>
+    <div
+      v-for="i in 3"
+      :key="i"
+      class="group"
+    >
+      <item-mod
+        :order="2"
+        enabled
+        name="Example Mod"
+        pack="Example Pack"
+        last-update="2024-06-01"
+      />
+      <hr class="h-px mx-2.5 border-gray-800 group-last:border-none">
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
