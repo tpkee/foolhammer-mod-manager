14904bb336c1e7674dc4dd0eb472493a1b8a6d13
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-08 18:32:56 +0100

    feat: add initial index page with template and state retrieval functionality

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      {{ t('hello_world') }}
+    </h1>
+    <button @click="getState">
+      Get State
+    </button>
+
+    <list-mods />
+  </div>
+</template>
+
+<script setup lang="ts">
+// When using the Tauri API npm package:
+const { t } = useI18n()
+
+const invoke = useInvoke()
+
+function getState() {
+  invoke('get_state').then((state) => {
+    console.log(state)
+  })
+}
+</script>
