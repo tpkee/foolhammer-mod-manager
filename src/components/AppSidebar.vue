a27a05be2399a75f5ee282450cffa7da8dfc0cd9
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-10 19:05:33 +0100

    refactor: remove unused header from AppSidebar component

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="min-h-screen p-2.5 border-l border-gray-800 space-y-2.5 flex flex-col justify-between items-center">
-    <h2>FHMM</h2>
-
     <div class="flex flex-col gap-1 grow">
       <sidebar-game v-for="(item, index) of games" :id="item" :key="index" :current-game="currentGame" />
     </div>
