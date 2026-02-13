fba9db9640306a7eb8703c71047ff9295ad40c8a
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-13 16:23:19 +0100

    feat: refactor index.vue to use getMods and getProfile for mod listing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,33 @@
 <template>
-  <div>
-    <list-mods :list="listAvailableMods" :loading="statusAvailableMods" />
+  <div class="space-y-2.5">
+    <list-mods :list="getMods" :profile="getProfile" @refresh="refresh()" />
   </div>
 </template>
 
 <script setup lang="ts">
+import type { GameResponseDto } from '~/types/dto'
+
 const preferencesStore = usePreferencesStore()
 const { t } = useI18n()
 
 // Fetching
-const { data: listAvailableMods, pending: statusAvailableMods } = await useAsyncData(`${preferencesStore.currentGame}-mods`, () => useTauriInvoke('get_mods'), {
-  default: () => [],
+const { data: selectedGame, refresh } = await useAsyncData<GameResponseDto>(`${preferencesStore.currentGame}-game`, () => useTauriInvoke('get_game', { gameId: preferencesStore.currentGame }), {
   watch: [() => preferencesStore.currentGame],
+})
+
+const getProfile = computed(() => {
+  if (!selectedGame.value || selectedGame.value.profiles?.length === 0)
+    return null
+
+  const first = selectedGame.value.profiles[0]
+
+  return selectedGame.value.profiles.find(p => p.default) ?? first
+})
+
+const getMods = computed(() => {
+  if (!getProfile.value)
+    return []
+
+  return getProfile.value.mods
 })
 </script>
