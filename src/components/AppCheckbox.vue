94b4fde461172cf3f1476eefa36d0cdedc8513b5
tpkee <89136677+tpkee@users.noreply.github.com>
2026-02-13 17:00:24 +0100

    refactor: improve AppCheckbox layout for better accessibility and styling

--- src/components/AppCheckbox.vue
+++ src/components/AppCheckbox.vue
@@ -1,13 +1,7 @@
 <template>
   <div :class="attrs.class">
-    <label :for="uid">
-      <span
-        v-if="label" :class="{
-          'sr-only': srOnlyLabel }"
-        class="block"
-      >
-        {{ label }}
-      </span>
+    <label :for="uid" class="flex items-center gap-1.5">
+
       <input
         :id="uid"
         v-model="modelValue"
@@ -15,6 +9,13 @@
         type="checkbox"
         class="size-3.5 appearance-none rounded-xs border border-gray-700 bg-gray-800 accent-purple-600 checked:appearance-auto"
       >
+
+      <span
+        v-if="label" :class="{
+          'sr-only': srOnlyLabel }"
+      >
+        {{ label }}
+      </span>
     </label>
   </div>
 </template>
