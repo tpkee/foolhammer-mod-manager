72279d6a031d2ec073bf49d40bebb33a0e3a9dfa
tpkee <89136677+tpkee@users.noreply.github.com>
2026-03-03 22:30:25 +0100

    refactor: replace header with breadcrumb cmp

--- src/pages/profiles.vue
+++ src/pages/profiles.vue
@@ -1,42 +1,32 @@
 <template>
-  <div>
-    <div class="space-y-4">
-      <!-- Breadcrumb -->
-      <div class="flex items-center gap-2">
-        <app-button variant="secondary" class="px-3 py-1.5" @click="goBack">
-          <div class="flex items-center gap-2">
-            <nuxt-icon name="mi:arrow-left" class="size-5" />
-            <span>Back</span>
-          </div>
-        </app-button>
-      </div>
+  <div class="space-y-4">
+    <app-breadcrump
+      :list="[{ label: 'Home', path: '/' }, { label: 'Profiles', path: '/profiles' }]"
+    />
 
-      <!-- Page Header -->
-      <div class="flex items-center justify-between">
-        <div>
-          <h1 class="text-2xl font-bold">
-            Profiles
-          </h1>
-          <p class="text-sm text-gray-400">
-            Manage your game profiles
-          </p>
-        </div>
-        <app-button class="px-4 py-2" @click="createModalRef?.open()">
-          Create New Profile
-        </app-button>
+    <div class="flex items-center justify-between">
+      <div>
+        <h1 class="text-2xl font-bold">
+          Profiles
+        </h1>
+        <p class="text-sm text-gray-400">
+          Manage your game profiles
+        </p>
       </div>
+      <app-button class="px-4 py-2" @click="createModalRef?.open()">
+        Create New Profile
+      </app-button>
+    </div>
 
-      <!-- Profiles List -->
-      <div class="space-y-2 max-h-[calc(100vh-12rem)] overflow-y-auto">
-        <item-profile
-          v-for="profile in gameStore.getProfiles"
-          :key="profile.name"
-          :profile="profile"
-          :is-active="profile.name === gameStore.getProfile?.name"
-          :game-id="gameStore.selectedGame!"
-          :all-profiles="gameStore.getProfiles"
-        />
-      </div>
+    <div class="space-y-2 max-h-[calc(100vh-12rem)] overflow-y-auto">
+      <item-profile
+        v-for="profile in gameStore.getProfiles"
+        :key="profile.name"
+        :profile="profile"
+        :is-active="profile.name === gameStore.getProfile?.name"
+        :game-id="gameStore.selectedGame!"
+        :all-profiles="gameStore.getProfiles"
+      />
     </div>
 
     <modal-create-profile
@@ -49,7 +39,6 @@
 </template>
 
 <script lang="ts" setup>
-const router = useRouter()
 const gameStore = useGameStore()
 
 const createModalRef = useTemplateRef('createModalRef')
@@ -57,8 +46,4 @@ const createModalRef = useTemplateRef('createModalRef')
 const getProfileNames = computed(() => gameStore.getProfiles.map(p => p.name))
 
 const refreshGame = inject('refreshGame') as () => void
-
-function goBack() {
-  router.back()
-}
 </script>
