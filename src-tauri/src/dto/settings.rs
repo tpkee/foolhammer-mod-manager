5929d0bf95ceaf660caf956bf72e11fe7721d1f4
tpkee <89136677+tpkee@users.noreply.github.com>
2026-03-04 23:48:47 +0100

    chore: remove the useless fields from the dto before the big beautiful refactor

--- src-tauri/src/dto/settings.rs
+++ src-tauri/src/dto/settings.rs
@@ -4,10 +4,6 @@ use crate::state::user_settings::{SettingKey, UserSettings};
 #[serde(rename_all = "camelCase")]
 pub struct UserSettingsResponseDto {
     pub default_game: Option<String>,
-    pub game_path: Option<String>,
-    pub steam_workshop_path: Option<String>,
-    pub saves_path: Option<String>,
-    pub mods_path: Option<String>,
 }
 
 impl From<&UserSettings> for UserSettingsResponseDto {
@@ -21,10 +17,6 @@ impl From<&UserSettings> for UserSettingsResponseDto {
 
         Self {
             default_game: get_str(&SettingKey::GameId),
-            game_path: get_str(&SettingKey::GamePath),
-            steam_workshop_path: get_str(&SettingKey::SteamWorkshopPath),
-            saves_path: get_str(&SettingKey::SavesPath),
-            mods_path: get_str(&SettingKey::ModsPath),
         }
     }
 }
