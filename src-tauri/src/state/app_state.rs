42697d2d06d501fb9c58ecfb219f02b77eb0cb97
tpkee <89136677+tpkee@users.noreply.github.com>
2026-03-05 01:31:31 +0100

    refactor: encapsulate watcher logic within FolderWatcher struct and keep track of old paths for removal

--- src-tauri/src/state/app_state.rs
+++ src-tauri/src/state/app_state.rs
@@ -3,32 +3,51 @@ use notify::{Event, RecursiveMode, Watcher};
 use std::path::PathBuf;
 use tauri::async_runtime::Mutex;
 
+struct FolderWatcher {
+    watcher: notify::RecommendedWatcher,
+    paths: Vec<PathBuf>,
+}
+
 pub struct State {
     pub game_runner: Option<Box<dyn GameManager>>,
-    watcher: notify::RecommendedWatcher,
+    folder_watcher: FolderWatcher,
 }
 
 pub type AppState<'a> = tauri::State<'a, Mutex<State>>;
 
 impl State {
-    pub fn set_watchers(&mut self, folders: &Vec<PathBuf>) {
-        let mut watcher_paths = self.watcher.paths_mut();
+    pub fn set_watchers(&mut self, folders: &[PathBuf]) {
+        let mut watcher_paths = self.folder_watcher.watcher.paths_mut();
+        let mut new_paths: Vec<PathBuf> = Vec::with_capacity(folders.len());
+        let old_paths = &self.folder_watcher.paths;
+
+        for old_path in old_paths {
+            if !folders.contains(old_path) || !old_path.exists() {
+                let _ = watcher_paths.remove(old_path);
+            }
+        }
 
         for folder in folders {
             if !folder.exists() {
                 continue;
             }
 
-            watcher_paths
-                .add(folder, RecursiveMode::Recursive)
-                .expect("Failed to watch folder from State::set_settings_from_store");
+            if !old_paths.contains(folder) {
+                watcher_paths
+                    .add(folder, RecursiveMode::Recursive)
+                    .expect("Failed to watch folder from State::set_watchers");
+            }
+
+            new_paths.push(folder.clone());
         }
 
         // TODO: this sucks, there should be a retry or smth like that for when it fails
         watcher_paths
             .commit()
             .is_ok()
-            .then(|| println!("Watching folders: {:?}", folders));
+            .then(|| println!("Watching folders: {:?}", new_paths));
+
+        self.folder_watcher.paths = new_paths;
     }
 }
 
@@ -38,7 +57,10 @@ impl Default for State {
             .expect("Failed to create watcher for State default");
 
         Self {
-            watcher,
+            folder_watcher: FolderWatcher {
+                watcher,
+                paths: vec![],
+            },
             game_runner: None,
         }
     }
