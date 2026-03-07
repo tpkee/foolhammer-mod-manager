b922d7dc0a56d5130b3a7601698f45fc03633d45
tpkee <89136677+tpkee@users.noreply.github.com>
2026-03-07 17:22:15 +0100

    refactor: move the mods commands to the profiles file

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,6 +1,5 @@
 pub mod games;
 pub mod groups;
 pub mod helpers;
-pub mod mods;
 pub mod profiles;
 pub mod settings;
--- src-tauri/src/commands/profiles.rs
+++ src-tauri/src/commands/profiles.rs
@@ -1,8 +1,10 @@
-use crate::commands::helpers::{modify_profile, modify_profiles};
-use crate::dto::profiles::ProfileRequestDto;
-use crate::stores::games::Profile;
-use crate::supported_games::SupportedGames;
-use crate::utils::ErrorCode;
+use crate::{
+    commands::helpers::{modify_profile, modify_profiles},
+    dto::{mods::ModRequestDto, profiles::ProfileRequestDto},
+    stores::games::{ModInfo, Profile},
+    supported_games::SupportedGames,
+    utils::ErrorCode,
+};
 
 #[tauri::command]
 pub async fn create_profile(
@@ -95,6 +97,63 @@ pub async fn toggle_manual_mode(
         profile.manual_mode = !profile.manual_mode;
 
         Ok(())
+    })
+    .await
+}
+
+#[tauri::command]
+pub async fn set_profile_mods(
+    app_handle: tauri::AppHandle,
+    game_id: SupportedGames,
+    profile_id: uuid::Uuid,
+    mods: Vec<ModRequestDto>,
+) -> Result<serde_json::Value, ErrorCode> {
+    modify_profile(&app_handle, game_id, profile_id, |profile| {
+        profile.mods = mods.into_iter().map(ModInfo::from).collect();
+        Ok(serde_json::json!(&profile.mods))
+    })
+    .await
+}
+
+#[tauri::command]
+pub async fn add_profile_mods(
+    app_handle: tauri::AppHandle,
+    game_id: SupportedGames,
+    profile_id: uuid::Uuid,
+    mods: Vec<ModRequestDto>,
+) -> Result<serde_json::Value, ErrorCode> {
+    modify_profile(&app_handle, game_id, profile_id, |profile| {
+        if profile.manual_mode {
+            let old_len = profile.mods.len();
+            let new_mods: Vec<ModInfo> = mods
+                .into_iter()
+                .enumerate()
+                .map(|(i, m)| ModInfo {
+                    name: m.name,
+                    enabled: m.enabled,
+                    order: m.order.unwrap_or(u32::try_from(old_len + i).unwrap_or(0)),
+                })
+                .collect();
+
+            profile.mods.extend(new_mods);
+        } else {
+            profile.mods.extend(mods.into_iter().map(ModInfo::from));
+        }
+        Ok(serde_json::json!(&profile.mods))
+    })
+    .await
+}
+
+#[tauri::command]
+pub async fn remove_profile_mods(
+    app_handle: tauri::AppHandle,
+    game_id: SupportedGames,
+    profile_id: uuid::Uuid,
+    mods: Vec<String>,
+) -> Result<serde_json::Value, ErrorCode> {
+    modify_profile(&app_handle, game_id, profile_id, |profile| {
+        profile.mods.retain(|m| !mods.contains(&m.name));
+        Ok(serde_json::json!(&profile.mods))
     })
     .await
 }
